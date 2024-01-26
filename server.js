ae48190750a660049370958b780ec0f960aa62a5
Abrham2119 <abrhamzewdu2119.com>
2024-01-26 21:14:37 +0300

    added the index file on server

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express= require('express');
 const app=express();
 
 app.get('/',(req,res)=>{
-    res.send('Hello');
+    res.send('world ');
 });
 
-app.listen(3003);
+app.listen(3005 ,function(){
+    console.log('Server is running on port 3005');
+});
