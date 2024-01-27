4b5e90ee6f4578e052d3ba588a54f1cc2f5edc2d
Abrham2119 <abrhamzewdu2119.com>
2024-01-27 07:44:32 +0300

    added the calculator file

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const express= require('express');
 const app=express();
 
 app.get('/',(req,res)=>{
-    res.send('world ');
+    res.sendFile(__dirname + '/index.html');
+    //console.log(__direname);
     
+
 });
 
 app.listen(3005 ,function(){
     console.log('Server is running on port 3005');
 });
+
+//expressjs.com
